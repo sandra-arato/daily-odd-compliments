3a7d9a62028c5d5d2ee2e38320e08f79f3068204
Sandra <sandra@Peters-MacBook-Air.local>
2014-01-28 12:54:32 -0500

    slight changes in fb call

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ window.fbAsyncInit = function() {
 	console.log("Facebook setup completed. Start fetching now...");
 
 	if (syncInit) {
-		FB.api( "/342691952483262/links/", function (response) {
+		FB.api( "/342691952483262/links/?key=value&access_token=516347275130300|9ca6946c431623ca202b9849b037b697", function (response) {
 			console.log("starting....");
 			if (response && !response.error) {
 				onsole.log("there is hope after all");
